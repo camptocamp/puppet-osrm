eb3952ab97ee73ce6e9f3e6b9da86033e7c0e3eb
Cédric Jeanneret <cedric.jeanneret@camptocamp.com>
2014-06-25 12:55:41 +0200

    creates osrm user and log directory — fixes issues #8 and #9

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,28 @@
-class osrm {
+# OSRM install
+class osrm ($ensure = present){
 
   if $::lsbdistcodename != wheezy {
-    fail "The ${module_name} module currently doesn't support ${::lsbdistcodename}"
+    fail "The ${module_name} module currently doesn't \
+    support ${::lsbdistcodename}"
+  }
+
+  $d_ensure = $ensure? {
+    present => 'directory',
+    default => $ensure
   }
 
   package {'osrm':
-    ensure => present,
+    ensure => $ensure,
+  }->
+  user {'osrm':
+    ensure => $ensure,
+    system => true,
+  }->
+  file {'/var/log/osrm':
+    ensure => $d_ensure,
+    owner  => 'osrm',
+    group  => 'osrm',
+    mode   => '0755',
   }
 
 }
